1eea4604d5d14f0a4dc89ffa917232a34a7cf795
Antonio Junior <antonio.silvajunior.as@gmail.com>
2022-08-29 13:34:59 -0300

    criando os tipos enumerados e suas prioridades

---
+++ src/main/java/com/antonio/pietroguerra/domain/enums/Perfil.java
@@ -0,0 +1,35 @@
+package com.antonio.pietroguerra.domain.enums;
+
+public enum Perfil {
+
+    ADMIN(0, "ROLE_ADMIN"), TECNICO(1, "ROLE_TECNICO"), VENDAS(2, "ROLE_VENDAS");
+
+    private Integer codigo;
+    private String descricao;
+
+    Perfil(Integer codigo, String descricao) {
+        this.codigo = codigo;
+        this.descricao = descricao;
+    }
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public static Perfil toEnum(Integer cod) {
+        if(cod == null) {
+            return null;
+        }
+        for(Perfil x : Perfil.values()) {
+            if(cod.equals(x.getCodigo())) {
+                return x;
+            }
+        }
+        throw new IllegalArgumentException("Perfil Inválido");
+    }
+
+}
