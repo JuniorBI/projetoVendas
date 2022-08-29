645cf638cd3934042252764dbc157251a78cc34e
Antonio Junior <antonio.silvajunior.as@gmail.com>
2022-08-29 13:20:05 -0300

    projeto iniciado

---
+++ src/main/java/com/antonio/pietroguerra/PietroGuerraApplication.java
@@ -0,0 +1,13 @@
+package com.antonio.pietroguerra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PietroGuerraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PietroGuerraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/antonio/pietroguerra/PietroGuerraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antonio.pietroguerra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PietroGuerraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
